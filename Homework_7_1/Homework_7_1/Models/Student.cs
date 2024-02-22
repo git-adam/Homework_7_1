23fad643874bfbf5064ef0f53e70f05f47d7e0da
git-adam <awwojcik1@gmail.com>
2024-02-22 13:28:33 +0100

    combobox binding

--- Homework_7_1/Homework_7_1/Models/Student.cs
+++ Homework_7_1/Homework_7_1/Models/Student.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Homework_7_1.Models
 {
-    class Student
+    public class Student
     {
+        public Student()
+        {
+            Group = new Group();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Comments { get; set; }
+        public string Math { get; set; }
+        public string Technology { get; set; }
+        public string Physics { get; set; }
+        public string PolishLang { get; set; }
+        public string ForeignLang { get; set; }
+        public bool Activities { get; set; }
+        public Group Group { get; set; }
     }
 }
--- Homework_7_1/Homework_7_1/ViewModels/MainViewModel.cs
+++ Homework_7_1/Homework_7_1/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Homework_7_1.Commands;
+using Homework_7_1.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +16,100 @@ namespace Homework_7_1.ViewModels
         public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+
+            Students = new ObservableCollection<Student>
+            {
+                new Student()
+                {
+                    FirstName = "Kazimierz",
+                    LastName = "Nowak",
+                    Group = new Group() { Id = 1 },
+                },
+
+                new Student()
+                {
+                    FirstName = "Marek",
+                    LastName = "Gaweł",
+                    Group = new Group() { Id = 2 },
+                },
+
+                new Student()
+                {
+                    FirstName = "Jacek",
+                    LastName = "Sztaba",
+                    Group = new Group() { Id = 1 },
+                },
+
+            };
+
+            InitGroups();
+
         }
+
         public ICommand RefreshStudentsCommand { get; set; }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get { return _selectedStudent; }
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get { return _students; }
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _selectedGroupId;
+
+        public int SelectedGroupId
+        {
+            get { return _selectedGroupId; }
+            set
+            {
+                _selectedGroupId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Group> _groups;
+
+        public ObservableCollection<Group> Groups
+        {
+            get { return _groups; }
+            set
+            {
+                _groups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void InitGroups()
+        {
+            Groups = new ObservableCollection<Group>()
+            {
+                new Group() {Id = 0, Name = "Wszystkie"},
+                new Group() {Id = 1, Name = "1A"},
+                new Group() {Id = 2, Name = "2A"}
+            };
+
+            SelectedGroupId = 0;
+        }
 
         private void RefreshStudents(object obj)
         {
-
         }
 
         private bool CanRefreshStudents(object obj)
